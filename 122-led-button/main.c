dcb87821d5971c8d92e68126498856990693ef6c
Суманова Виктория <visumanova@gmail.com>
2026-09-16 20:59:56 +0300

    Сделана п1.2.2

--- 122-led-button/main.c
+++ 122-led-button/main.c
@@ -1,24 +1,35 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
-#include "hardware/regs/addressmap.h"
-#include "hardware/regs/sio.h"
 
 const uint LED_PIN = 25;
+const uint BUTTON_PIN = 15;
 
 int main()
 {
-   gpio_init(LED_PIN);
-   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-   volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
-   volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
-   const uint32_t led_mask = 1u << LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-   while (1)
-   {
-       *gpio_out_set = led_mask;
-       sleep_ms(250);
-       *gpio_out_clr = led_mask;
-       sleep_ms(1000);
-   }
+
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+   
+    bool led = false;
+    bool previous = false;
+
+    while (1)
+    {
+        bool current = gpio_get(BUTTON_PIN);
+
+        // Ловим момент нажатия: было true (не нажата), стало false (нажата)
+        if (previous == true && current == false)
+        {
+            led = !led;
+            gpio_put(LED_PIN, led);
+        }
+
+        previous = current;
+    }
 }
